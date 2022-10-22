2d982ffb1c3a569e2fd4c77cbeda5ddcae006501
Diego Niemeyer <102764313+dniemeryers@users.noreply.github.com>
2022-10-22 14:44:15 -0300

    creat
    creat
    
    Co-Authored-By: Diego <766917+diego@users.noreply.github.com>

---
+++ js/setup.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true ,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            
+            items:2
+                       
+        }
+    }
+})
